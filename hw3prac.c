d486f15df8cfe13696d3681799d3dba0324a9bd3
balthaszar <76969035+balthaszar@users.noreply.github.com>
2021-03-08 15:49:29 -0500

    Add files via upload

--- hw3prac.c
+++ hw3prac.c
@@ -13,15 +13,17 @@ void decimalToOct(int decValue, char octString[]);
 
 int main() {
 
-	//creating variable for name
-	char name[32], valueString[100];
+	//creating variable for namee
+	char name[32], valueString[100], valueString1[100], valueString2[100];
 	//take in user's name
 	printf("Enter your name: ");
 	fgets(name,sizeof(name),stdin);
 
 	int numb = getInteger();
+	decimalToHex(numb, valueString1);
+	decimalToOct(numb, valueString2);
 	decimalToBinary(numb, valueString);
-
+	
 	
 
 
@@ -83,9 +85,10 @@ void decimalToBinary(int decValue, char binString[]) {
 		quotient = quotient / 2;
 	}
 
-	printf("Binary: ");
+	if(decValue>0)
+		printf("Binary: ");
 
-
+	
 	for(j = i-1; j>0;j--)
 		printf("%d", binString[j]);
 	printf("\n");
@@ -93,4 +96,49 @@ void decimalToBinary(int decValue, char binString[]) {
 	
 }
 
+void decimalToHex(int decValue, char hexString[]) {
+	long int quotient;
+	int i = 1, j, temp;
 
+	quotient = decValue;
+
+	while(quotient!=0) {
+		temp = quotient % 16;
+
+		//convert integer into character
+		if(temp<10)
+			temp = temp + 48;
+		else
+			temp = temp + 55;
+
+		hexString[i++] = temp;
+		quotient = quotient / 16;
+	}
+
+	
+	if(decValue>0)
+		printf("Hexadecimal: ");
+
+	for(j= i-1; j > 0; j--)
+		printf("%c", hexString[j]);
+	printf("\n");
+}
+
+
+void decimalToOct(int decValue, char octString[]) {
+	int quotient, i = 1, j, temp;
+
+	quotient = decValue;
+
+	while(quotient!=0) {
+		octString[i++] = quotient % 8;
+		quotient = quotient / 8;
+	}
+	if(decValue>0)
+                printf("Octal: ");
+
+
+        for(j = i-1; j>0;j--)
+                printf("%d", octString[j]);
+        printf("\n");
+}
